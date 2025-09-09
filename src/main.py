41206965fe65eeb24e3061d936e6353d6f72c721
oniyudi <onimaru.yudi@gmail.com>
2025-09-09 13:20:55 -0300

    colocando o prefix e habilitando rotas

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
+from routes import data_routes
 
 app = FastAPI()
+app.include_router(data_routes.router, prefix="/api")
 
 @app.get("/")
 def read_root():
