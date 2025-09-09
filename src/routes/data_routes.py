7dd7594ace8cb8989a07136310df4ceb5ab94623
oniyudi <onimaru.yudi@gmail.com>
2025-09-09 13:16:16 -0300

    criando routers testes

---
+++ src/routes/data_routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router = APIRouter(tags=["data"])
+
+@router.get("/data/")
+def get_data():
+    return {"lista com os dados"}
+
+@router.get("/data/{item_id}")
+def get_single_item(item_id: int):
+    return {f"Exibindo as informações do item {item_id}"}
