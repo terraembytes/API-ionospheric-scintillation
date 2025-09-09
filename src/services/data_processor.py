82dcf4d0f8a975c7ca5085466ee5ac6d4406bb40
oniyudi <onimaru.yudi@gmail.com>
2025-09-09 12:32:55 -0300

    iniciando o data_processor e add env example

---
+++ src/services/data_processor.py
@@ -0,0 +1,8 @@
+import requests
+# https://api-ismrquerytool.fct.unesp.br/
+
+url = "https://api-ismrquerytool.fct.unesp.br/"
+
+url_token = url + "api/v1/user/token"
+url_get_data = url + "api/v1/data/download/ismr/file"
+
