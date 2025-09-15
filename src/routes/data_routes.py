d7c9c61a2020c1d12b9645e6087b8e136d232b20
oniyudi <onimaru.yudi@gmail.com>
2025-09-15 15:06:41 -0300

    iniciando a segunda rota

--- src/routes/data_routes.py
+++ src/routes/data_routes.py
@@ -5,6 +5,12 @@ from services.data_processor import IsmrQueryToolAPIClient, get_ISMR_API_client
 
 router = APIRouter(tags=["data"])
 
+params = {
+    'start': str,
+    'end': str,
+    'station': str
+}
+
 @router.get("/data/{start}/{end}/{station}")
 async def get_data(
     start: str,
@@ -12,11 +18,14 @@ async def get_data(
     station: str,
     api_client: Annotated[IsmrQueryToolAPIClient, Depends(get_ISMR_API_client)]
 ):
+    params.start = start
+    params.end = end
+    params.station = station
     print('Buscando os dados...')
     data = await api_client.get_dados(start=start, end=end, station=station)
     processed_data = [{"Date": item['time_utc'], 'Svid': item['svid'], 'S4': item['s4'], 'Elevation': item['elev']} for item in data.get('data', [])]
     return {'data': processed_data}
 
-@router.get("/data/{item_id}")
+@router.get("/data/{start}/{end}/{station}?elev={elev}")
 def get_single_item(item_id: int):
     return {f"Exibindo as informações do item {item_id}"}
