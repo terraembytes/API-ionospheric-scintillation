6471ce12296c1468d8006c061e51e4f8617e1ab2
oniyudi <onimaru.yudi@gmail.com>
2025-09-29 00:09:16 -0300

    habilitando o localhost do frontend

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 from typing import Union
 from fastapi import FastAPI
 from routes import data_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Lista de origens permitidas
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # Permite as origens da lista
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos os métodos (GET, POST, etc)
+    allow_headers=["*"], # Permite todos os cabeçalhos
+)
+
 app.include_router(data_routes.router, prefix="/api")
 
 @app.get("/")
