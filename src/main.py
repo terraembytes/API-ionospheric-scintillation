e743143233e8118626d10a5d8ba0999922dcbd6c
oniyudi <onimaru.yudi@gmail.com>
2025-11-12 14:12:43 -0300

    acrescentando outras origins

--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
 ]
 
 app.add_middleware(
