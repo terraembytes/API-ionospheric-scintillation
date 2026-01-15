20d622f01be5f25cad34cf0712993552a84e9e41
oniyudi <onimaru.yudi@gmail.com>
2026-01-14 21:25:12 -0300

    otimizando as rotas e arrumando a verificação de dados existentes

--- src/routes/data_routes.py
+++ src/routes/data_routes.py
@@ -14,54 +14,22 @@ params = {
 
 dados = None
 
-# rota padrão para os dados gerais
-@router.get("/data/")
-async def get_data(
-    start: str,
+# rota padrão, podendo receber query params
+@router.get("v1/data/")
+async def get_datas(
+    elev: int, 
+    elevType: int, 
+    constellation: str, 
+    start: str, 
     end: str, 
-    station: str,
+    station: str, 
     api_client: Annotated[IsmrQueryToolAPIClient, Depends(get_ISMR_API_client)]
-):
+    ):
     global dados
-    if verificar_parametros_iguais(params, start, end, station):
-        print('retornando dados repetidos')
-        return {'data': dados}
-    else:
-        params['start'] = start
-        params['end'] = end
-        params['station'] = station
-        print('Buscando os dados...')
-        try:
-            data = await api_client.get_dados(start=start, end=end, station=station)
-            processed_data = [{"Date": item['time_utc'], 'Svid': item['svid'], 'S4': item['s4'], 'Elevation': item['elev'], 'Azimute': item['azim'], 'Intensity': item['avg_cn0_l1']} for item in data.get('data', [])]
-            dados = processed_data
-            return {'data': dados}
-        except ReadTimeout: # demorou muito para responder
-            raise HTTPException(
-                status_code=504, 
-                detail="A API externa (ISMR) demorou muito para responder."
-            )
-        except ConnectionError: # não conseguiu se conectar
-            raise HTTPException(
-                status_code=503,
-                detail="Não foi possível conectar com a API externa (ISMR)"
-            )
-        except Exception as e:
-            raise HTTPException(
-                status_code=500,
-                detail=f"Erro ao processar os dados: {e}"
-            )
-
-
-# rota de filtragem dos dados gerais
-@router.get("/data/filters/geral/")
-async def filter_geral_datas(elev: int, elevType: int, constellation: str, start: str, end: str, station: str, api_client: Annotated[IsmrQueryToolAPIClient, Depends(get_ISMR_API_client)]):
-    global dados
-    if dados != None:
+    if dados != None and (params['start'] == start and params['end'] == end and params['station'] == station):
         data_filtered = filter_constella_elev(dados, constellation, elev, elevType)
         return {'data': data_filtered}
     else:
-        data_filtered = dados
         params['start'] = start
         params['end'] = end
         params['station'] = station
@@ -89,10 +57,10 @@ async def filter_geral_datas(elev: int, elevType: int, constellation: str, start
             )
 
 # rota para o grafico de contagem do indice S4
-@router.get("/data/filters/contGraphs/")
+@router.get("v1/data/contGraphs/")
 async def filter_cont_s4(elev: int, elevType: int, constellation: str, time: str, start: str, end: str, station: str, api_client: Annotated[IsmrQueryToolAPIClient, Depends(get_ISMR_API_client)]):
     global dados
-    if dados != None:
+    if dados != None and (params['start'] == start and params['end'] == end and params['station'] == station):
         data_filtered2 = filter_constella_elev(dados, constellation, elev, elevType)
         print("Agrupando os valores de S4...")
         data_filtered3 = group_s4(data_filtered2, constellation, time)
