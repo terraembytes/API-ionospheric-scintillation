ef805ace57c341cf0a55c9df923a5ee5ab0d63e1
oniyudi <onimaru.yudi@gmail.com>
2026-02-13 11:45:18 -0300

    feat: nova rota skyplot com poligonos

--- src/routes/data_routes.py
+++ src/routes/data_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from typing import Annotated
 from services.data_processor import IsmrQueryToolAPIClient, get_ISMR_API_client
-from utils.helpers import verificar_parametros_iguais, group_s4, filter_constella_elev, cut_hour_range
+from utils.helpers import group_s4, filter_constella_elev, cut_hour_range, constellation_filter, get_s4_higher_equals, transform_to_radian, convert_to_xyScale, create_cluster, get_polygons
 from httpx import ReadTimeout
 from services.temporary_memory import DataService, get_data_service
 import traceback
@@ -75,6 +75,46 @@ async def filter_cont_s4(
         print("Agrupando os valores de S4...")
         data_filtered3 = group_s4(data_filtered2, constellation, time)
         return {'data': data_filtered3}
+    except ReadTimeout:
+        raise HTTPException(
+            status_code=504, 
+            detail="A API externa (ISMR) demorou muito para responder."
+        )
+    except ConnectionError: # não conseguiu se conectar
+        raise HTTPException(
+            status_code=503,
+            detail="Não foi possível conectar com a API externa (ISMR)"
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Erro ao processar os dados: {e}"
+        )
+    
+@router.get("v1/data/skyplot-polygon/")
+async def get_skyplot_polygon(
+    constellation: str, 
+    start: str, 
+    end: str, 
+    station: str, 
+    api_client: Annotated[IsmrQueryToolAPIClient, Depends(get_ISMR_API_client)],
+    service: Annotated[DataService, Depends(get_data_service)],
+    hour_range: int | None = Query(default=None, alias="hourRange"),
+    date_selected: str | None = Query(default=None, alias="dateSelected")
+):
+    try:
+        dados_brutos = await service.get_data(api_client, start, end, station)
+
+        data_cut = cut_hour_range(hour_range, date_selected, dados_brutos)
+        data_constellation = constellation_filter(constellation, data_cut)
+        data_s4 = get_s4_higher_equals(0.6, data_constellation)
+        data_values_converted = transform_to_radian(data_s4)
+        xy_scale = convert_to_xyScale(data_values_converted)
+        clusters = create_cluster(xy_scale, 9, 5)
+        list_polygons = get_polygons(clusters, xy_scale)
+
+        return {'data': {'data': data_constellation, 'polygons': list_polygons}}
+
     except ReadTimeout:
         raise HTTPException(
             status_code=504, 
