ba4287e94359ec10e887f604911cf222e4ead2cc
oniyudi <onimaru.yudi@gmail.com>
2026-03-02 14:19:02 -0300

    feat: add rota grafico skyplot com plot baseado no S4

--- src/routes/data_routes.py
+++ src/routes/data_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from typing import Annotated
 from services.data_processor import IsmrQueryToolAPIClient, get_ISMR_API_client
-from utils.helpers import verificar_parametros_iguais, group_s4, filter_constella_elev, cut_hour_range
+from utils.helpers import group_s4, filter_constella_elev, cut_hour_range, add_size_s4, convert_str_to_float, convert_number_to_str, remover_s4_nan
 from httpx import ReadTimeout
 from services.temporary_memory import DataService, get_data_service
 import traceback
@@ -38,6 +38,47 @@ async def get_datas(
         data_cut = cut_hour_range(hour_range, date_selected, dados_brutos)
         data_filtered2 = filter_constella_elev(data_cut, constellation, elev, elevType)
         return {'data': data_filtered2}
+    except ReadTimeout:
+        raise HTTPException(
+            status_code=504, 
+            detail="A API externa (ISMR) demorou muito para responder."
+        )
+    except ConnectionError: # não conseguiu se conectar
+        raise HTTPException(
+            status_code=503,
+            detail="Não foi possível conectar com a API externa (ISMR)"
+        )
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(
+            status_code=500,
+            detail=f"Erro ao processar os dados: {e}"
+        )
+    
+@router.get("/v1/data/skyplotS4/")
+async def get_skiplot_s4_data(
+    elev: int, 
+    elevType: int, 
+    constellation: str, 
+    start: str, 
+    end: str, 
+    station: str, 
+    api_client: Annotated[IsmrQueryToolAPIClient, Depends(get_ISMR_API_client)],
+    service: Annotated[DataService, Depends(get_data_service)],
+    hour_range: int | None = Query(default=None, alias="hourRange"),
+    date_selected: str | None = Query(default=None, alias="dateSelected")
+):
+    try:
+        # Chama o método do serviço importado
+        dados_brutos = await service.get_data(api_client, start, end, station)
+
+        data_cut = cut_hour_range(hour_range, date_selected, dados_brutos)
+        data_filtered2 = filter_constella_elev(data_cut, constellation, elev, elevType)
+        data_number_converted = convert_str_to_float(data_filtered2, 'S4')
+        processed_data = remover_s4_nan(data_number_converted)
+        data_with_size = add_size_s4(processed_data)
+        data_reconverted = convert_number_to_str(data_with_size, 'S4')
+        return {'data': data_reconverted}
     except ReadTimeout:
         raise HTTPException(
             status_code=504, 
