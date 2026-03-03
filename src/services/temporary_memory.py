be30544f518850d6497f53b8ba001a3f020619ba
oniyudi <onimaru.yudi@gmail.com>
2026-03-03 10:43:40 -0300

    refactor: memoria temporaria otimizada para fazer menos requisicoes a API ISMR

--- src/services/temporary_memory.py
+++ src/services/temporary_memory.py
@@ -7,6 +7,7 @@ class DataService:
     def __init__(self):
         # Cache armazena: {(start, end, station): dados_processados}
         self._cache: Dict[tuple, list] = {}
+        self._cache_lock = asyncio.Lock()
         # Cache token para armazenar o token da requisicao
         self._cache_token: str | None = None
         self._token_expires_at: datetime | None = None
@@ -21,36 +22,37 @@ class DataService:
     async def get_data(self, api_client: IsmrQueryToolAPIClient, start: str, end: str, station: str):
         cache_key = (start, end, station)
 
-        if cache_key in self._cache:
-            print(f"Cache hit: Usando dados em memória para {cache_key}")
-            return self._cache[cache_key]
+        async with self._cache_lock:
+            if cache_key in self._cache:
+                print(f"Cache hit: Usando dados em memória para {cache_key}")
+                return self._cache[cache_key]
 
-        print(f"Cache miss: Buscando na API ISMR para {cache_key}...")
-        try:
-            async with self._token_lock:
-                if self._is_token_expired is None or self._is_token_expired():
-                    print("Buscando novo token...")
-                    self._cache_token, self._token_expires_at = await api_client._get_token()
+            try:
+                print(f"Cache miss: Buscando na API ISMR para {cache_key}...")
+                async with self._token_lock:
+                    if self._is_token_expired is None or self._is_token_expired():
+                        print("Buscando novo token...")
+                        self._cache_token, self._token_expires_at = await api_client._get_token()
 
-            data = await api_client.get_dados(start=start, end=end, station=station, token=self._cache_token)
-            
-            processed_data = [
-                {
-                    "Date": item.get('time_utc'), 
-                    'Svid': item.get('svid'), 
-                    'S4': item.get('s4'), 
-                    'Elevation': item.get('elev'), 
-                    'Azimute': item.get('azim'), 
-                    'Intensity': item.get('avg_cn0_l1')
-                } 
-                for item in data.get('data', [])
-            ]
-            
-            self._cache[cache_key] = processed_data
-            return processed_data
+                data = await api_client.get_dados(start=start, end=end, station=station, token=self._cache_token)
+        
+                processed_data = [
+                    {
+                        "Date": item.get('time_utc'), 
+                        'Svid': item.get('svid'), 
+                        'S4': item.get('s4'), 
+                        'Elevation': item.get('elev'), 
+                        'Azimute': item.get('azim'), 
+                        'Intensity': item.get('avg_cn0_l1')
+                    } 
+                    for item in data.get('data', [])
+                ]
+                
+                self._cache[cache_key] = processed_data
+                return processed_data
 
-        except Exception as e:
-            raise e
+            except Exception as e:
+                raise e
 
 data_service = DataService()
 
